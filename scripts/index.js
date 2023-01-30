1652dbbe1826687dc50120f76619584bd3716ef6
ArrowVulcan <stalkeer@hotmail.com>
2023-01-30 23:11:37 +0100

    Adding project to gihtub.
    Adding the Arkaik Guide project to github.

---
+++ scripts/index.js
@@ -0,0 +1,191 @@
+var swordsman_quest = [
+    {
+        "title": "Quest Swordsman - (1/2)",
+        "map": "izlude",
+        "x": 247,
+        "y": 304,
+        "description": "Use <span class='npc'>[Kafra]</span> and warp to Payon."
+    },
+    {
+        "title": "Quest Swordsman - (1/2)",
+        "map": "payon",
+        "x": 370,
+        "y": 20,
+        "description": "Walk to the portal at the top right of the map."
+    },
+    {
+        "title": "Quest Swordsman - (1/2)",
+        "map": "pay_arche",
+        "x": 65,
+        "y": 154,
+        "description": "Walk to the payon dungeon portal."
+    },
+    {
+        "title": "Quest Swordsman - (1/2)",
+        "map": "pay_dun00",
+        "description": "Kill <span class='monster'>[Skeleton]</span> until you find a <span class='item'>[Gem of Skeleton]</span>."
+    },
+    {
+        "title": "Quest Swordsman - (1/2)",
+        "map": "izlude",
+        "x": 71,
+        "y": 195,
+        "description": "Go back and talk to <span class='npc'>[Hans]</span>.<br><span class='hint'>Use ability <span class='skill'>[Return]</span> to instantly get back (Unless you have saved return point somewhere else)</span>"
+    },
+    {
+        "title": "Quest Swordsman - (2/2)",
+        "map": "izlude",
+        "x": 247,
+        "y": 304,
+        "description": "Use <span class='npc'>[Kafra]</span> and warp to Zero."
+    },
+    {
+        "title": "Quest Swordsman - (2/2)",
+        "map": "zero",
+        "x": 238,
+        "y": -5,
+        "description": "Walk to the portal at the top of the map."
+    },
+    {
+        "title": "Quest Swordsman - (2/2)",
+        "map": "zero_field",
+        "x": 104,
+        "y": 358,
+        "description": "Find <span class='npc'>[Hans]</span> next to the tree and talk to him. You are now a Swordsman!"
+    },
+]
+
+var knight_quest = [
+    {
+        "title": "Job Change",
+        "map": "izlude",
+        "x": 225,
+        "y": 44,
+        "description": "Enter the portal to Knights Academy at the top of Izlude."
+    },
+    {
+        "title": "Job Change",
+        "description": "Walk straight up until you find <span class='npc'>[Saibah]</span> and talk to her."
+    },
+    {
+        "title": "Quest Knight (1/7)",
+        "map": "zero",
+        "x": 351,
+        "y": 345,
+        "description": "Go back to Zero and enter the building and talk to <span class='npc'>[Recrutador]</span>."
+    },
+    {
+        "title": "Quest Knight (1/7)",
+        "map": "zero",
+        "x": 188,
+        "y": 285,
+        "description": "Talk to <span class='npc'>[Encarregado dos Esgotos]</span> and enter the sewer.<br><span class='hint'>You can talk with <span class='npc'>[Recrutador]</span> and he will teleport you there.</span>"
+    },
+    {
+        "title": "Quest Knight (1/7)",
+        "map": "prt_sewb1",
+        "description": "Kill 50 <span class='monster'>[Thief Bug]</span>, 30 <span class='monster'>[Thief Bug Female]</span> and 5 <span class='monster'>[Thief Bug Egg]</span>."
+    },
+    {
+        "title": "Quest Knight (1/7)",
+        "map": "izlude",
+        "x": 225,
+        "y": 44,
+        "description": "Go back to Izlude and enter the portal to Knights Academy at the top of Izlude."
+    },
+    {
+        "title": "Quest Knight (1/7)",
+        "description": "Walk straight up and talk with <span class='npc'>[Saibah]</span> again."
+    },
+    {
+        "title": "Quest Knight (2/7)",
+        "map": "morocc",
+        "x": 236,
+        "y": 457,
+        "description": "Go to Morocc and enter the portal at the center bottom of the map."
+    },
+    {
+        "title": "Quest Knight (2/7)",
+        "map": "moc_fild12",
+        "x": 18,
+        "y": 92,
+        "description": "Enter the portal on the left side of the map."
+    },
+    {
+        "title": "Quest Knight (2/7)",
+        "map": "cmd_fild08",
+        "x": 404,
+        "y": 25,
+        "description": "Enter the portal to Infernal Anthill."
+    },
+    {
+        "title": "Quest Knight (2/7)",
+        "map": "anthell01",
+        "x": 425,
+        "y": 52,
+        "description": "Talk to <span class='npc'>[Felt]</span> at the top right corner."
+    },
+    {
+        "title": "Quest Knight (3/7)",
+        "map": "anthell01",
+        "description": "Kill 20 <span class='monster'>[Andre]</span>, 10 <span class='monster'>[Vitata]</span> and 15 <span class='monster'>[Piere]</span>."
+    },
+    {
+        "title": "Quest Knight (3/7)",
+        "map": "anthell01",
+        "x": 425,
+        "y": 52,
+        "description": "Go back and talk to <span class='npc'>[Felt]</span> at the top right corner."
+    },
+    {
+        "title": "Quest Knight (4/7)",
+        "map": "izlude",
+        "x": 225,
+        "y": 44,
+        "description": "Go back to Izlude and enter the portal to Knights Academy at the top of Izlude."
+    },
+    {
+        "title": "Quest Knight (4/7)",
+        "description": "Walk straight up and talk with <span class='npc'>[Saibah]</span> again."
+    },
+    {
+        "title": "Quest Knight (5/7)",
+        "map": "izlude",
+        "x": 247,
+        "y": 304,
+        "description": "Use <span class='npc'>[Kafra]</span> and warp to Zero."
+    },
+    {
+        "title": "Quest Knight (5/7)",
+        "map": "zero",
+        "x": 219,
+        "y": 288,
+        "description": "Talk to <span class='npc'>[Kafra]</span> and warp to Alberta."
+    },
+    {
+        "title": "Quest Knight (5/7)",
+        "map": "alberta",
+        "x": 433,
+        "y": 333,
+        "description": "Talk to <span class='npc'>[Princess Hina]</span>."
+    },
+    {
+        "title": "Quest Knight (5/7)",
+        "description": "Talk to <span class='npc'>[Princess Hina]</span> again on the right side of the boat."
+    },
+    {
+        "title": "Quest Knight (6/7)",
+        "description": "Kill 2 <span class='monster'>[Swordfish]</span> on the boat."
+    },
+    {
+        "title": "Quest Knight (7/7)",
+        "map": "izlude",
+        "x": 225,
+        "y": 44,
+        "description": "Go back to Izlude and enter the portal to Knights Academy at the top of Izlude."
+    },
+    {
+        "title": "Quest Knight (7/7)",
+        "description": "Walk straight up and talk with <span class='npc'>[Saibah]</span>. You are now a Knight!"
+    },
+]
