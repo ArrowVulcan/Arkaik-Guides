9fb8b981cc1d67fdc5616eede31e15a0af4da5fc
ArrowVulcan <stalkeer@hotmail.com>
2023-01-31 17:06:12 +0100

    Added Acolyte Job Change
    Acolyte Job quest guide is now added to the project.

--- scripts/index.js
+++ scripts/index.js
@@ -189,3 +189,113 @@ var knight_quest = [
         "description": "Walk straight up and talk with <span class='npc'>[Saibah]</span>. You are now a Knight!"
     },
 ]
+
+var acolyte_quest = [
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "zero_field",
+        "x": 236,
+        "y": 466,
+        "description": "After exiting the church, head toward the Zero city at the bottom of the Zero Florest map."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "zero",
+        "x": 219,
+        "y": 288,
+        "description": "Talk to <span class='npc'>[Kafra]</span> and choose warp to Alberta."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "alberta",
+        "x": 9,
+        "y": 58,
+        "description": "Go to the portal at the top left of Alberta."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_fild03",
+        "x": 430,
+        "y": 328,
+        "description": "Talk with the either the first <span class='npc'>[demon]</span> and play a mini-game of snake or talk with the chatroom 'Alternative' <span class='npc'>[demon]</span> and kill 5 <span class='monster'>[Zombie]</span> in Payon dungeon.<br><span class='hint'>This guide will use the alternative quest path. If you decided to play the mini-game and completed it, skip to step 15.</span>"
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_fild03",
+        "x": 470,
+        "y": 384,
+        "description": "Walk back into Alberta city."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "alberta",
+        "x": 42,
+        "y": 45,
+        "description": "Talk to <span class='npc'>[Kafra]</span> and choose warp to Payon."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "payon",
+        "x": 369,
+        "y": 21,
+        "description": "Go to the portal at the top right of Payon."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_arche",
+        "x": 65,
+        "y": 154,
+        "description": "Walk to the payon dungeon portal."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_dun00",
+        "description": "Kill 5 <span class='monster'>[Zombie]</span>.<br><span class='hint'>I recommend to kill 1 <span class='monster'>[Skeleton]</span> and collect a <span class='item'>Mace [4]</span>.</span>"
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_dun00",
+        "x": 34,
+        "y": -7,
+        "description": "Go out of the dungeon with the portal at the top left."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_arche",
+        "x": 187,
+        "y": 448,
+        "description": "Walk to Payon city by using the portal at the bottom of the map."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "payon",
+        "x": 289,
+        "y": 339,
+        "description": "Talk to <span class='npc'>[Kafra]</span> and choose warp to Alberta."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "alberta",
+        "x": 9,
+        "y": 58,
+        "description": "Go to the portal at the top left of Alberta."
+    },
+    {
+        "title": "Quest Acolyte - (1/2)",
+        "map": "pay_fild03",
+        "x": 430,
+        "y": 328,
+        "description": "Talk with the 'Alternative' <span class='npc'>[demon]</span>.</span>"
+    },
+    {
+        "title": "Quest Acolyte - (2/2)",
+        "map": "pay_fild03",
+        "x": 430,
+        "y": 328,
+        "description": "Use ability <span class='skill'>[Return]</span> to instantly get back to the church.<br><span class='hint'>If you have saved a return point somewhere else, find a <span class='npc'>[Kafra]</span> and teleport to Zero, enter Zero Florest with the portal at the top center of the map. On the Zero Florest map, you will find the church at the top right of the map.</span>"
+    },
+    {
+        "title": "Quest Acolyte - (2/2)",
+        "description": "Talk with <span class='npc'>[Sasha White]</span>. You are now a Acolyte!"
+    },
+]
